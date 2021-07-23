8b7aec0a7b780129940a597437a4f7f2ac5d73cd
NemesLaszlo <wow.laszlo@gmail.com>
2021-07-23 20:11:19 +0200

    Excel file read methods

---
+++ ExcelRead.cs
@@ -0,0 +1,140 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Data;
+using System.IO;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelReadTest
+{
+    public static class ExcelRead
+    {
+        /// <summary>
+        /// Read an Excel file row by row
+        /// </summary>
+        /// <param name="FilePath">Path to the excel file</param>
+        public static void readXLS(string FilePath)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            FileInfo existingFile = new FileInfo(FilePath);
+            using (ExcelPackage package = new ExcelPackage(existingFile))
+            {
+                //get the first worksheet in the workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                int colCount = worksheet.Dimension.End.Column;  //get Column Count
+                int rowCount = worksheet.Dimension.End.Row;     //get row count
+
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    for (int col = 1; col <= colCount; col++)
+                    {
+                        Console.WriteLine(" Row:" + row + " column:" + col + " Value:" + worksheet.Cells[row, col].Value.ToString().Trim());
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Read an Excel file row by row using the excel app itself (Office interop) 
+        /// </summary>
+        /// <param name="FilePath">Path to the excel file</param>
+        public static void getExcelFile(string FilePath)
+        {
+
+            //Create COM Objects. Create a COM object for everything that is referenced
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(FilePath);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            //iterate over the rows and columns and print to the console as it appears in the file
+            //excel is not zero based!!
+            for (int i = 2; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    //new line
+                    if (j == 1)
+                        Console.Write("\r\n");
+
+                    //write the value to the console
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                        Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+                }
+            }
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        /// <summary>
+        /// /Reads an Excel file and converts it into dataset with each sheet as each table of the dataset
+        /// </summary>
+        /// <param name="FilePath">Path to the excel file</param>
+        /// <param name="headers">If set to true the first row will be considered as headers</param>
+        /// <returns></returns>
+        public static DataSet DatasetImportFromExcel(string FilePath, bool headers = true)
+        {
+            var _xl = new Excel.Application();
+            var wb = _xl.Workbooks.Open(FilePath);
+            var sheets = wb.Sheets;
+            DataSet dataSet = null;
+            if (sheets != null && sheets.Count != 0)
+            {
+                dataSet = new DataSet();
+                foreach (var item in sheets)
+                {
+                    var sheet = (Excel.Worksheet)item;
+                    DataTable dt = null;
+                    if (sheet != null)
+                    {
+                        dt = new DataTable();
+                        dt.TableName = sheet.Name;
+                        var ColumnCount = ((Excel.Range)sheet.UsedRange.Rows[1, Type.Missing]).Columns.Count;
+                        var rowCount = ((Excel.Range)sheet.UsedRange.Columns[1, Type.Missing]).Rows.Count;
+
+                        for (int j = 0; j < ColumnCount; j++)
+                        {
+                            var cell = (Excel.Range)sheet.Cells[1, j + 1];
+                            var column = new DataColumn(headers ? cell.Value : string.Empty);
+                            dt.Columns.Add(column);
+                        }
+
+                        for (int i = 0; i < rowCount; i++)
+                        {
+                            var r = dt.NewRow();
+                            for (int j = 0; j < ColumnCount; j++)
+                            {
+                                var cell = (Excel.Range)sheet.Cells[i + 1 + (headers ? 1 : 0), j + 1];
+                                r[j] = cell.Value;
+                            }
+                            dt.Rows.Add(r);
+                        }
+
+                    }
+                    dataSet.Tables.Add(dt);
+                }
+            }
+            _xl.Quit();
+            return dataSet;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+
+namespace ExcelReadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelRead.readXLS(@"C:\Users\Laci\Desktop\Test.xlsx");
+
+
+            Console.WriteLine("--------------------------------------------------");
+
+
+            ExcelRead.getExcelFile(@"C:\Users\Laci\Desktop\Test.xlsx");
+
+
+            Console.WriteLine("\n--------------------------------------------------");
+
+
+
+            DataSet DataSet = ExcelRead.DatasetImportFromExcel(@"C:\Users\Laci\Desktop\Test.xlsx");
+
+            Console.WriteLine("Table counts: " + DataSet.Tables.Count);
+
+            // Example to check and get the Table name into a variable
+            if (DataSet.Tables.Contains("Sheet1"))
+            {
+                string name = DataSet.Tables["Munka1"].TableName;
+                Console.WriteLine("First Sheet name: " + name);
+            }
+
+            foreach (DataRow row in DataSet.Tables["Sheet1"].Rows) // DataSet.Tables[0].Rows is good as well
+            {
+                object[] rowData = row.ItemArray;
+                foreach (var i in rowData)
+                {
+                    Console.WriteLine(i.ToString());
+                }
+            }
+
+            Console.WriteLine("Other sheet: ");
+
+            foreach (DataRow row in DataSet.Tables["Sheet2"].Rows)
+            {
+                object[] rowData = row.ItemArray;
+                foreach (var i in rowData)
+                {
+                    Console.WriteLine(i.ToString());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
